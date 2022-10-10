e91e940c28eaa943c680d44b6e0d4681a9d3e150
Jacques Tessier <jacques.tessier17@yahoo.com>
2022-10-10 14:53:04 +0200

    MAJ Obj console

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,18 +1,21 @@
 package test;
 
 import metier.Console;
+import metier.Hybride;
 import metier.Jeu;
+import metier.Portable;
+import metier.Salon;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Console c1 = new Console("DS");
-		Console c2 = new Console("PS2");
-		Console c3 = new Console("SWITCH");
-		Console c4 = new Console("XBOX");
-		Console c5 = new Console("PSP");
+		Portable c1 = new Portable("DS");
+		Salon c2 = new Salon("PS2");
+		Hybride c3 = new Hybride("SWITCH");
+		Salon c4 = new Salon("XBOX");
+		Portable c5 = new Portable("PSP");
 		
 		Jeu j1 = new Jeu("Pokemon" , c1);
 		Jeu j2 = new Jeu("God of War", c2);
